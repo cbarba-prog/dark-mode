4302e09caeeaf9dca0b4a83a1e37890af8f4c26e
Your Name <you@example.com>
2021-01-14 00:37:36 -0800

    MVP

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,10 @@
+
+// import { useState } from "react";
+import useLocalStorage from "./useLocalStorage";
+
+const useDarkMode = () => {
+  const [someValue, setSomeValue] = useLocalStorage("itsDark");
+  return [someValue, setSomeValue];
+};
+
+export default useDarkMode;
